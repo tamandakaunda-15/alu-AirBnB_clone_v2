0dbe61743d974afd4fab5e92c7c6567dab8381d3
tamandakaunda-15 <tamandakaunda900@gmail.com>
2024-06-14 19:56:20 +0000

    deploy static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,39 +1,27 @@
 #!/usr/bin/env bash
-# This script sets up my web servers for the deployment of web_static
+# script that sets up web servers for the deployment of web_static
+sudo apt-get update
+sudo apt-get -y install nginx
+sudo ufw allow 'Nginx HTTP'
 
-# server names (actual IP addresses)
-export SERVER_NAME_1="3.91.244.104"
-export SERVER_NAME_2="204.236.213.151"
-
-# Install Nginx if it's not already installed
-if ! dpkg -s nginx >/dev/null 2>&1; then
-    sudo apt-get update
-    sudo apt-get install -y nginx
-fi
-
-# Create directories if they don't exist
-sudo mkdir -p /data/web_static/releases/test
-sudo mkdir -p /data/web_static/shared
-
-# Create a fake HTML file
-echo "<html>
+sudo mkdir -p /data/
+sudo mkdir -p /data/web_static/
+sudo mkdir -p /data/web_static/releases/
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
+sudo touch /data/web_static/releases/test/index.html
+sudo echo "<html>
   <head>
   </head>
   <body>
     Holberton School
   </body>
-</html>" | sudo tee /data/web_static/releases/test/index.html >/dev/null
+</html>" | sudo tee /data/web_static/releases/test/index.html
 
-# Create a symbolic link
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
 
-# Set ownership of /data/ directory
 sudo chown -R ubuntu:ubuntu /data/
 
-sudo sed -i "/^\tlocation \/hbnb_static/ {
-    \n\t\talias /data/web_static/current;\n\t}
-}" /etc/nginx/sites-available/default
+sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
 
 sudo service nginx restart
-
-curl http://localhost/hbnb_static
