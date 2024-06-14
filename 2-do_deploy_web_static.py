c19d8418ab81440af3788d6e0a5f3a75bbece419
tamandakaunda-15 <tamandakaunda900@gmail.com>
2024-06-14 20:03:12 +0000

    python

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-# Fabfile to distribute an archive to a web server.
+""" 
+Fabfile to distribute an archive to a web server.
+"""
+
 import os.path
 from fabric.api import env
 from fabric.api import put
